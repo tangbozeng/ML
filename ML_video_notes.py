a7cf19d16b95b073faac87a54555dce310931e1e
BT <tangbozeng@users.noreply.github.com>
2018-11-25 19:29:23 +0000

    Update ML_video_notes.py

--- ML_video_notes.py
+++ ML_video_notes.py
@@ -1,1 +1,13 @@
-
+1.supervised learning (classification):
+  decision tree
+  Nesrest Neighbor
+  Support Vector Machine
+  Neural Network
+2.supervised learning (regression)
+  Linear Regression
+  Non-linear regression
+2.upspervised learning:
+  K-means
+  hierachical clustring
+  PCA
+  
